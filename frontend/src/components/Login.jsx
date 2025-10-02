ba1032217e070bddb2d5ece29cd41fa296d573bb
gamza <aprille138@gmail.com>
2025-10-02 20:28:10 +0900

    feat: login

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="LoginBackGround">
+      <div className="Wrapper">
+        <div className="Logo">BreadFeet</div>
+        <div className="Phrase">빵지순례를 위한 단 하나의 준비물</div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
