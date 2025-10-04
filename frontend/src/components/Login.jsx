cd7d10a2ad75f36c0436efd7d2aa42320977e57a
gamza <aprille138@gmail.com>
2025-10-04 19:16:47 +0900

    feat: kakao login button

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,6 @@
 import "./Login.css";
+import KakaoLoginWide from "../assets/kakao_login_large_wide.png";
+import KakaoLoginNarrow from "../assets/kakao_login_large_narrow.png";
 
 const Login = () => {
   return (
@@ -7,6 +9,14 @@ const Login = () => {
         <div className="Logo">BreadFeet</div>
         <div className="Phrase">빵지순례를 위한 단 하나의 준비물</div>
       </div>
+      <a herf="#!">
+        <picture>
+          <source srcSet={KakaoLoginNarrow} media="(max-width: 1024px)" />{" "}
+          {/* 1024px 이하일때 narrow ver로 변경 */}
+          <img src={KakaoLoginWide} alt="카카오 로그인" />
+          {/* 기본은 wide ver */}
+        </picture>
+      </a>
     </div>
   );
 };
