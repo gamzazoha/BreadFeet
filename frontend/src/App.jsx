41c5c945fcd6aa2af2a906a64a21a15c55cd9a0d
gamza <aprille138@gmail.com>
2025-10-05 17:08:33 +0900

    feat: make navigation bar ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="Navbar">
+      <nav>
+        <ul className="NavMenu">
+          <li>
+            <NavLink to="/">HOME</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">MAP</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">EVENT</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Login">LOGIN</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
